fe910536216dca6798006fa9f2c099d063c52df4
timteplych <timteplych@mail.ru>
2018-06-27 17:12:32 +0800

    added users and auth service

--- src/main/java/ru/ttv/jchatttv/BaseAuthService.java
+++ src/main/java/ru/ttv/jchatttv/BaseAuthService.java
@@ -1,11 +1,27 @@
 package ru.ttv.jchatttv;
 
+import java.util.ArrayList;
+
 public class BaseAuthService implements AuthService {
+    private ArrayList<User> users;
+
+    public BaseAuthService(){
+        users = new ArrayList<User>();
+        users.add(new User("login1","pass1",  "nick1"));
+        users.add(new User("login2","pass2",  "nick2"));
+        users.add(new User("login3","pass3",  "nick3"));
+    }
+
     public void start() {
 
     }
 
     public String getNickByLoginPass(String login, String pass) {
+        for (User user: users) {
+            if(user.getLogin().equals(login) && user.getPass().equals(pass)){
+                return user.getNick();
+            }
+        }
         return null;
     }
 
--- src/main/java/ru/ttv/jchatttv/User.java
+++ src/main/java/ru/ttv/jchatttv/User.java
@@ -4,5 +4,22 @@ public class User {
     private String login;
     private String pass;
     private String nick;
-    
+
+    public User(String login, String pass, String nick){
+        this.login = login;
+        this.pass = pass;
+        this.nick = nick;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public String getNick() {
+        return nick;
+    }
 }
